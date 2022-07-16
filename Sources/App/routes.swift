dc824fa4565c2257f377223470b461acef550b08
Tim <0xtimc@gmail.com>
2022-07-16 01:34:14 +0100

    Show requests being spread out

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,13 +3,17 @@ import Vapor
 import ClientRateLimiter
 
 func routes(_ app: Application) throws {
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "y-MM-dd H:mm:ss.SSSS"
+    
     app.get { req in
         return "It works!"
     }
 
     app.get("hello") { req async throws -> String in
         let response = try await req.clientRateLimiter.get("https://www.google.com")
-        req.logger.info("Time: \(Date()): \(response.status)")
+        req.logger.info("Time: \(dateFormatter.string(from: Date())): \(response.status)")
         return "Hello, world!"
     }
 
